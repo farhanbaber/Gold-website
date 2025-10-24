3dca49a585019971c256f3f11251ea1e0566fdb3
farhan baber <farhanbaber965@gmail.com>
2025-10-25 00:04:06 +0500

    done

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import styles from './Home.module.css';
+import Collections from './Collections';
 
 const Home = () => {
   return (
     <div>
-            {/* <h1>helo this is Home</h1> */}
+     <div className={styles["heroDev"]}>
+  <div className={styles["hero-content"]}>
+    <p className={styles["hero-para"]}>Personalized jewellery 2026</p>
+    <h1 className={styles["hero-title"]}>
+      Elegance <span>jewellery</span>
+    </h1>
+    <button className={styles["hero-button"]}>Shop Collections</button>
+  </div>
+</div>
 
     </div>
   )
